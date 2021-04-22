08869c5b532a00ba715e0179b45ab233eca2ccd7
Ahmet Kağan Yörük <ahmetkagan@hotmail.com>
2021-04-22 10:38:52 +0300

    <

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
+import { Camera } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as FaceDetector from 'expo-face-detector';
 
 export default function App() {
+  const [detect, setDetect] = useState('red');
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      console.log('izin verildi');
+    })();
+  }, []);
+
+  const handleFacesDetected = ({ faces }) => {
+    if (faces.length === 1 && (faces[0].yawAngle > 350 || faces[0].yawAngle < 5) && (faces[0].rollAngle > 350 || faces[0].rollAngle < 5)) {
+      console.log(faces[0]);
+      setDetect('green');
+    } else {
+      setDetect('red');
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: detect }}>
+      <Camera
+        style={{ flex: 1 }}
+        type={Camera.Constants.Type.front}
+        ratio="1:1"
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.Constants.Mode.accurate,
+          detectLandmarks: FaceDetector.Constants.Landmarks.none,
+          runClassifications: FaceDetector.Constants.Classifications.none,
+          tracking: true,
+        }}
+      />
+      <View style={{ flex: 1 }} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
