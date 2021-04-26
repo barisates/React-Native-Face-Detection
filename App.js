ebead718a1ef073db566f817c8fb988676b05bf3
bates <bates@trabilisim.com>
2021-04-26 14:49:57 +0300

    Development
    Face tracking has been added, the rule structure has been dynamic.

--- App.js
+++ App.js
@@ -1,68 +1,65 @@
 import { Camera } from 'expo-camera';
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import * as FaceDetector from 'expo-face-detector';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const colorArray = ['green', 'red', 'blue', 'purple', 'orange', 'yellow', 'brown', 'black', 'white', 'gray'];
+
+const conditions = [
+  { message: 'Kafanızı sağa çeviriniz.', rule: face => face && (face.yawAngle < 65 && face.yawAngle > 20) && (face.rollAngle > 350 || face.rollAngle < 20) },
+  { message: 'Sol gözünüzü kapayınız.', rule: face => face && (face.yawAngle < 20 || face.yawAngle > 350) && (face.rollAngle > 330 || face.rollAngle < 20) && face.rightEyeOpenProbability < 0.2 && face.leftEyeOpenProbability > 0.8 },
+  { message: 'Gülümseyiniz.', rule: face => face && face.smilingProbability > 0.95 },
+  { message: 'Canlısınız.' },
+];
 
 export default function App() {
-  const [detect, setDetect] = useState('Kafanızı sağa çeviriniz');
+  const [step, setStep] = useState(0);
+  const [detetPosition, setDetectPosition] = useState([{
+    height: 0,
+    width: 0,
+    left: 0,
+    top: 0,
+  }]);
 
   const handleFacesDetected = face => {
-    if (detect === 'Kafanızı sağa çeviriniz'
-    && face
-    && (face.yawAngle < 65 && face.yawAngle > 20)
-    && (face.rollAngle > 350 || face.rollAngle < 20)) {
-      setDetect('Sol gözünüzü kapayınız');
-    } else if (detect === 'Sol gözünüzü kapayınız'
-    && face
-    && (face.yawAngle < 20 || face.yawAngle > 350)
-    && (face.rollAngle > 330 || face.rollAngle < 20)
-    && face.rightEyeOpenProbability < 0.2
-    && face.leftEyeOpenProbability > 0.8
-    ) {
-      setDetect('Gülümseyiniz');
-    } else if (detect === 'Gülümseyiniz'
-    && face
-    && face.smilingProbability > 0.95
-    ) { setDetect('tamam'); }
+    if (face) {
+      setDetectPosition(face.map(item => ({
+        height: item.bounds.size.height,
+        width: item.bounds.size.width,
+        left: item.bounds.origin.x,
+        top: item.bounds.origin.y,
+      })));
+
+      if (step < 3 && conditions[step].rule(face[0])) {
+        setStep(step + 1);
+      }
+    }
   };
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      console.log('izin verildi');
+      await Camera.requestPermissionsAsync();
     })();
   }, []);
   return (
-    <View style={{ flex: 1, backgroundColor: detect }}>
-      <Camera
-        style={{ flex: 1, zIndex: 2 }}
-        type={Camera.Constants.Type.front}
-        ratio="16:9"
-        onFacesDetected={e => handleFacesDetected(e.faces[0])}
-        faceDetectorSettings={{
-          mode: FaceDetector.Constants.Mode.accurate,
-          detectLandmarks: FaceDetector.Constants.Landmarks.none,
-          runClassifications: FaceDetector.Constants.Classifications.all,
-          tracking: true,
-        }}
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 0.9, zIndex: 1 }}>
+        <Camera
+          style={{ flex: 1, zIndex: -1 }}
+          type={Camera.Constants.Type.front}
+          ratio="16:9"
+          onFacesDetected={e => handleFacesDetected(e.faces)}
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.accurate,
+            detectLandmarks: FaceDetector.Constants.Landmarks.none,
+            runClassifications: FaceDetector.Constants.Classifications.all,
+            tracking: true,
+          }}
 
-      />
-
-      <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 30, justifyContent: 'flex-end', alignItems: 'center' }}>
-        <View style={{ backgroundColor: 'white', paddingHorizontal: 30, paddingVertical: 10 }}>
-          <Text style={{ fontWeight: '900', fontSize: 15, color: 'black' }}>
-            {detect}
-            {' '}
-          </Text>
-        </View>
-
+        />
       </View>
-      <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }}>
-        <View style={{ backgroundColor: 'transparent', borderWidth: 1, width: windowWidth / 2, height: windowWidth / 1.8, borderRadius: 100, transform: [{ scaleY: 1.3 }] }} />
-
+      {detetPosition.map((item, index) => <View style={{ position: 'absolute', borderWidth: 2, borderColor: colorArray[index], height: item.height, width: item.width, left: item.left, top: item.top, zIndex: 99, borderRadius: 25 }} />)}
+      <View style={{ flex: 0.1, alignContent: 'center', justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ textAlign: 'center', textAlignVertical: 'center', fontSize: 20 }}>{conditions[step].message}</Text>
       </View>
     </View>
   );
